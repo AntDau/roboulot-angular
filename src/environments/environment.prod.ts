f7d73285ba464deed46bebed7497d0a1df158ba5
AntDau <anthonydauphin17@gmail.com>
2023-01-05 13:48:22 +0100

    modification fichier d'environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
-  production: false,
-  offres_api_express: 'http://localhost:8000/api/offres'
+  production: true,
+  //offres_api_express: 'http://localhost:8000/api/offres',
+  offres_api_express: 'https://node-api-offres.herokuapp.com/',
+  user_api: 'http://localhost:8080/zoboulot/api/user',
+  compte_api: 'http://localhost:8080/zoboulot/compte',
+  AUTH_API: 'http://localhost:8080/zoboulot/api/auth/'
 };
