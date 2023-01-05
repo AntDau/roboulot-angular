a6d092977301d83cd8f13b74e4a4158ee344574b
AntDau <anthonydauphin17@gmail.com>
2023-01-05 15:02:33 +0100

    ajout variable d'environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,10 @@ export const environment = {
   production: true,
   //offres_api_express: 'http://localhost:8000/api/offres',
   offres_api_express: 'https://node-api-offres.herokuapp.com/',
-  user_api: 'http://localhost:8080/zoboulot/api/user',
-  compte_api: 'http://localhost:8080/zoboulot/compte',
-  AUTH_API: 'http://localhost:8080/zoboulot/api/auth/'
+  //user_api: 'http://localhost:8080/zoboulot/api/user',
+  user_api: 'https://spring-roboulot.herokuapp.com/zoboulot/api/user',
+  //compte_api: 'http://localhost:8080/zoboulot/compte',
+  compte_api: 'https://spring-roboulot.herokuapp.com/zoboulot/compte',
+  //AUTH_API: 'http://localhost:8080/zoboulot/api/auth/'
+  AUTH_API: 'https://spring-roboulot.herokuapp.com/zoboulot/api/auth/'
 };
